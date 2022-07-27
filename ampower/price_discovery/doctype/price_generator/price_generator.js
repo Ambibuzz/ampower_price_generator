b306f598c4cc27a648fd805996ed4b11511e06e1
rajatambi <rajat.tripathi@ambibuzz.com>
2022-07-27 05:56:31 +0000

    initial commint for Price Discovery

---
+++ ampower/price_discovery/doctype/price_generator/price_generator.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, n ithead@ambibuzz.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Price Generator', {
+	// refresh: function(frm) {
+
+	// }
+});
